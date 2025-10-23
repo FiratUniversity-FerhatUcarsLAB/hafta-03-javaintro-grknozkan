4ceff7b9e7d900165adb2b330c85171aa07dbfda
grknozkan <cozkan653@gmail.com>
2025-10-23 20:19:57 +0300

    CarpimTablosu.java

---
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -0,0 +1,17 @@
+public class CarpımTablosu {
+    public static void main(String[] args){
+        int a=2;
+        System.out.println(" 2'nin katları");
+        System.out.println("===============");
+        System.out.println("2x1 = "+(a*1));
+        System.out.println("2x2 = "+(a*2));
+        System.out.println("2x3 = "+(a*3));
+        System.out.println("2x4 = "+(a*4));
+        System.out.println("2x5 = "+(a*5));
+        System.out.println("2x6 = "+(a*6));
+        System.out.println("2x7 = "+(a*7));
+        System.out.println("2x8 = "+(a*8));
+        System.out.println("2x9 = "+(a*9));
+        System.out.println("2x10 = "+(a*10));
+    }
+}
