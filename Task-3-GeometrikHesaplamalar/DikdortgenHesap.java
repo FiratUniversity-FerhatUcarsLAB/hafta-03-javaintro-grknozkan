128c90cae99e931b090a92023ad9242a2cc581a4
grknozkan <cozkan653@gmail.com>
2025-10-23 20:14:47 +0300

    DikdortgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,10 @@
+public class Dikdortgen {
+    public static void main(String[] args){
+        double en=4.5;
+        double boy=7.9;
+        double alan=(en*boy);
+        double cevre=(en+boy)*2;
+        System.out.println("Alan:"+alan);
+        System.out.println("Çevre:"+cevre);
+    }
+}
