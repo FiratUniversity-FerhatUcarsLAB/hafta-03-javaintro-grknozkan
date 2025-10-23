1b37dacfce24cbc4c889e29c7f6aa3d8c85836ae
grknozkan <cozkan653@gmail.com>
2025-10-23 20:19:20 +0300

    MilKilometreTablosu.java

---
+++ Task-5-TabloDesenleri/MilKilometreTablosu.java
@@ -0,0 +1,10 @@
+public class MilHesabi {
+    public static void main (String[] args){
+        int mil1=1 ,mil2=5 ,mil3=10,mil4=20,mil5=50 ;
+        System.out.println("1 mil="+(mil1*1609)+" km");
+        System.out.println("5 mil="+(mil2*1609)+" km");
+        System.out.println("10 mil="+(mil3*1609)+" km");
+        System.out.println("20 mil="+(mil4*1609)+" km");
+        System.out.println("50 mil="+(mil5*1609)+" km");
+    }
+}
