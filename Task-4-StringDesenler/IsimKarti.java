b22cc7783f8ddbc7d693bac030bbfbdbbb39ed0f
grknozkan <cozkan653@gmail.com>
2025-10-23 20:16:21 +0300

    IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,16 @@
+public class İsimKarti {
+    public static void main(String[] args) {
+        String ad = "Gürkan";
+        String soyad = "Özkan";
+        String ogrNo = "250542023";
+        String bolum = "Yazılım Mühendisliği";
+        System.out.println("*******************");
+        System.out.println(" ÖGRENCİ BİLGİLERİ ");
+        System.out.println("*******************");
+        System.out.println("Ad: " + ad);
+        System.out.println("Soyad : " + soyad);
+        System.out.println("No: " + ogrNo);
+        System.out.println("Bolum : " + bolum);
+        System.out.println("*******************");
+    }
+}
