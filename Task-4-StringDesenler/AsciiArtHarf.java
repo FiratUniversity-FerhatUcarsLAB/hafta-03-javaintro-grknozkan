99c328d15c3624a27e760c0376bdea208566b4bc
grknozkan <cozkan653@gmail.com>
2025-10-23 20:16:57 +0300

    AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,13 @@
+public class ASCG {
+    public static void main(String[] args){
+        System.out.println("////////////////////");
+        System.out.println("/");
+        System.out.println("/");
+        System.out.println("/");
+        System.out.println("/          /////////");
+        System.out.println("/                  /");
+        System.out.println("/                  /");
+        System.out.println("/                  /");
+        System.out.println("////////////////////");
+    }
+}
