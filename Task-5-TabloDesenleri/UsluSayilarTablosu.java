6ca983ce4be7335728f7602520096217935b83bc
grknozkan <cozkan653@gmail.com>
2025-10-23 20:18:14 +0300

    UsluSayilarTablosu.java

---
+++ Task-5-TabloDesenleri/UsluSayilarTablosu.java
@@ -0,0 +1,12 @@
+public class UsluSayi {
+    public static void main(String[] args) {
+        System.out.println("    a      a^2      a^3");
+        System.out.println("-------------------------");
+        int a1=1 ,a2=2 , a3=3,a4=4,a5=5;
+        System.out.println("    "+a1+"       "+(a1*a1)+"        "+(a1*a1*a1));
+        System.out.println("    "+a2+"       "+(a2*a2)+"        "+(a2*a2*a2));
+        System.out.println("    "+a3+"       "+(a3*a3)+"        "+(a3*a3*a3));
+        System.out.println("    "+a4+"       "+(a4*a4)+"       "+(a4*a4*a4));
+        System.out.println("    "+a5+"       "+(a5*a5)+"       "+(a5*a5*a5));
+    }
+}
