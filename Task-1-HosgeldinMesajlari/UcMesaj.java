1cf81982b59508ec7996c5811faeb4827ce65ba8
grknozkan <cozkan653@gmail.com>
2025-10-23 20:09:16 +0300

    UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,8 @@
+public class UcMesaj {
+    public static void main(String[] args) {
+        System.out.println("Javaya Hos Geldiniz" );
+        System.out.println("Bilgisayar Bilimlerine Hos Geldiniz" );
+        System.out.println("Programla eglencidir");
+    }
+
+}
