bd59758297dccf82baa117c700170eeca0a55e25
grknozkan <cozkan653@gmail.com>
2025-10-23 20:11:45 +0300

    IkiIslem.java

---
+++ Task-2-Matematikselislemler/IkiIslem.java
@@ -0,0 +1,9 @@
+public class İkiİslemSonucu {
+    public static void main(String[] args){
+        double pay=(9.5*4*5-2.5*3);
+        double payda=45.5-3;
+        double sonuc = pay / payda;
+        System.out.printf("%.4f\n", sonuc);
+        }
+
+    }
