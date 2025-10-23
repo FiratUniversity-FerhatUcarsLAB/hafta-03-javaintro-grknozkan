eda7a5d5145734e6e14d6f2552b5abaaa1ad3bdd
grknozkan <cozkan653@gmail.com>
2025-10-23 20:07:30 +0300

    BesKereTekrar.java

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,9 @@
+public class Beskeztekrar {
+    public static void main(String[] args){
+        System.out.println("Javaya Hos Geldiniz");
+        System.out.println("Javaya Hos Geldiniz");
+        System.out.println("Javaya Hos Geldiniz");
+        System.out.println("Javaya Hos Geldiniz");
+        System.out.println("Javaya Hos Geldiniz");
+    }
+}
